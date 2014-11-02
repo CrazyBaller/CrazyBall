3fc071e163dd7d5ec109551cbd179758eb80735d
hdyycx <448352274@qq.com>
2014-11-02 11:16:24 +0800

    duan next test

--- CrazyBall2/src/com/edu/seu/message/Data.java
+++ CrazyBall2/src/com/edu/seu/message/Data.java
@@ -19,14 +19,8 @@ public class Data {
     
     public static int flag=0;
     public static ArrayList<Float> location = new ArrayList<Float>();
-    public static ArrayList<Integer> boardsize = new ArrayList<Integer>();
-    
-    /**  state:  0 未准备 ; 1 已准备; 2 游戏中; 3 失败;  4 胜利**/
 	public static ArrayList<Integer> state = new ArrayList<Integer>();
-	
 	public static ArrayList<Float> ball = new ArrayList<Float>();
-	public static int ballsize = 0 ;
-	
 	public static int myID = 0;
 	public static String hostID = null;
     
@@ -34,7 +28,8 @@ public class Data {
     public static float board2_x=0;
     public static float board2_y=SCREEN_WIDTH-2*board_halfheight;
     
-	public Data(){
+    
+    public Data(){
     	if(flag==0){
     		flag=1;
    		 float def = 0;
@@ -43,13 +38,10 @@ public class Data {
    		
    		location.add(def);
    		state.add(0);
-   		boardsize.add(0);
    		}
    		
    		ball.add(def);
    		ball.add(def);
-   		
-   		ballsize=0;
        	}
     }
    
@@ -61,11 +53,9 @@ public class Data {
 			{
 			state.set(i, 0);
 			location.set(i,def);
-			boardsize.add(0);
 			}
 		ball.set(0, def);
 		ball.set(1, def);
-		ballsize=0;
 	}
 	public  void setMode (int mode)
 	{
